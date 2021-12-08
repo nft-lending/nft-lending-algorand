708ab8b602c4eedb922acfa52b2b564a3fa6e6dd
jordan-public <jordan.stojanovski@gmail.com>
2021-12-08 05:40:39 -0500

    Splt forms

--- webapp/src/components/Borrower.js
+++ webapp/src/components/Borrower.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Form, Button, Card } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap'
 import Balances from './Balances'
 import algosdk from 'algosdk'
 import signSendAwait from '../util/signSendAwait'
@@ -25,7 +25,7 @@ function Borrower(props) {
             return compiledBytes;
     }
 
-    const onStartAuction = async () => {
+    const onCreateAuction = async () => {
         const approvalProgram = await compileProgram(nftloan_approval.default)
         const clearProgram = await compileProgram(nftloan_clear_state.default)
 
@@ -40,7 +40,7 @@ function Borrower(props) {
         appArgs.push(algosdk.encodeUint64(Math.floor(loanAmount * 1000000)))
         appArgs.push(algosdk.encodeUint64(Math.floor(maxRepayAmount * 1000000)))
         appArgs.push(algosdk.encodeUint64(Math.floor(decrFactor*DENOMINATOR)))
-console.log(appArgs)
+
         const txn = algosdk.makeApplicationCreateTxn(
             props.account.address,
             params,
@@ -55,17 +55,50 @@ console.log(appArgs)
         )
         //const txId = auctionContractCreateTxn.txID().toString();
         const confirmedTxn = signSendAwait(txn, props.wallet, props.algodClient, props.refreshAccountInfo)
-        if (confirmedTxn) setApplicationIndex(confirmedTxn.applicationIndex)
+        if (confirmedTxn) {
+            console.log("Created aucton: " + confirmedTxn.applicationIndex)
+            window.alert("Created auction: " + confirmedTxn.applicationIndex)
+        }
     }
 
-    const onDeleteAuction = async () => {
+    const onStartAuction = async () => {
+        const params = await props.algodClient.getTransactionParams().do()
+
+        const appArgs = [];
+        appArgs.push(algosdk.encodeObj("start"))
+
+        // Create block with payments
+
+        // create unsigned transaction
+        // !!!TODO: modify this
+        const txn = algosdk.makeApplicationDeleteTxn(props.account.address, params, applicationIndex, appArgs);
+
+        signSendAwait(txn, props.wallet, props.algodClient, props.refreshAccountInfo)
+    }
+
+    const onCancelAuction = async () => {
         const params = await props.algodClient.getTransactionParams().do()
 
         const appArgs = [];
         appArgs.push(algosdk.encodeObj("cancel"))
-console.log(appArgs)
+
         // create unsigned transaction
-        const txn = algosdk.makeApplicationDeleteTxn(props.account.address, params, 50005888, appArgs);
+        const txn = algosdk.makeApplicationDeleteTxn(props.account.address, params, applicationIndex, appArgs);
+
+        signSendAwait(txn, props.wallet, props.algodClient, props.refreshAccountInfo)
+    }
+
+    const onRepayAuction = async () => {
+        const params = await props.algodClient.getTransactionParams().do()
+
+        const appArgs = [];
+
+        // TODO: add payment transactions
+
+        appArgs.push(algosdk.encodeObj("repay"))
+
+        // create unsigned transaction
+        const txn = algosdk.makeApplicationDeleteTxn(props.account.address, params, applicationIndex, appArgs);
 
         signSendAwait(txn, props.wallet, props.algodClient, props.refreshAccountInfo)
     }
@@ -74,7 +107,7 @@ console.log(appArgs)
         const params = await props.algodClient.getTransactionParams().do()
 
         // create unsigned transaction
-        const txn = algosdk.makeApplicationOptInTxn(props.account.address, params, 49982576);
+        const txn = algosdk.makeApplicationOptInTxn(props.account.address, params, applicationIndex);
 
         signSendAwait(txn, props.wallet, props.algodClient, props.refreshAccountInfo)
     }
@@ -84,65 +117,92 @@ console.log(appArgs)
 
         // create unsigned transaction 
         // TODO: ompare to makeApplicationCloseOutTxn 
-        const txn = algosdk.makeApplicationClearStateTxn(props.account.address, params, 49982576);
+        const txn = algosdk.makeApplicationClearStateTxn(props.account.address, params, applicationIndex);
 
         signSendAwait(txn, props.wallet, props.algodClient, props.refreshAccountInfo)
     }
     return (<>
         <Balances algodClient={props.algodClient} account={props.account} accountInfo={props.accountInfo} refreshAccountInfo={props.refreshAccountInfo} wallet={props.wallet} />
 
-        <Card border="primary" style={{ width: '18rem' }}>
-            <Card.Header>Auction: {applicationIndex}</Card.Header>
-            <Card.Body>
-                <Form>
-                    <Form.Group className="mb-3">
-                        <Form.Label>NFT ID</Form.Label>
-                        <Form.Control type="number" onChange={e => setNFTId(parseInt(e.target.value))} placeholder="49055308" />
-                    </Form.Group>
+        <Container fluid="md">
+            <Row>
+                <Col>
+                    <Card border="primary" style={{ width: '18rem' }}>
+                        <Card.Header>Auction:</Card.Header>
+                        <Card.Body>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>NFT ID</Form.Label>
+                                    <Form.Control type="number" onChange={e => setNFTId(parseInt(e.target.value))} placeholder="49055308" />
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Loan amount</Form.Label>
-                        <Form.Control type="number" onChange={e => setLoanAmount(parseFloat(e.target.value))} placeholder="100.0" />
-                    </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Loan amount</Form.Label>
+                                    <Form.Control type="number" onChange={e => setLoanAmount(parseFloat(e.target.value))} placeholder="100.0" />
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Max repay amount</Form.Label>
-                        <Form.Control type="number" onChange={e => setMaxRepayAmount(parseFloat(e.target.value))} placeholder="120.0" />
-                    </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Max repay amount</Form.Label>
+                                    <Form.Control type="number" onChange={e => setMaxRepayAmount(parseFloat(e.target.value))} placeholder="120.0" />
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Min repay decrease factor</Form.Label>
-                        <Form.Control type="number" onChange={e => setdecrFactor(parseFloat(e.target.value))} placeholder="0.9" />
-                    </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Min repay decrease factor</Form.Label>
+                                    <Form.Control type="number" onChange={e => setdecrFactor(parseFloat(e.target.value))} placeholder="0.9" />
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Auction Duration (seconds)</Form.Label>
-                        <Form.Control type="number" onChange={e => setAuctionDuration(parseInt(e.target.value))} placeholder="10" />
-                    </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Auction Duration (seconds)</Form.Label>
+                                    <Form.Control type="number" onChange={e => setAuctionDuration(parseInt(e.target.value))} placeholder="10" />
+                                </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Repaymen Duration (seconds)</Form.Label>
-                        <Form.Control type="number" onChange={e => setRepayDuration(parseInt(e.target.value))} placeholder="50" />
-                    </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Repayment Duration (seconds)</Form.Label>
+                                    <Form.Control type="number" onChange={e => setRepayDuration(parseInt(e.target.value))} placeholder="50" />
+                                </Form.Group>
 
-                    <Button variant="primary" onClick={onStartAuction}>
-                        Start auction
-                    </Button>
-                    <br/><br/>
-                    <Button variant="primary" onClick={onOptIn}>
-                        Opt In
-                    </Button>
-                    <br/><br/>
-                    <Button variant="primary" onClick={onClearAuction}>
-                        Clear
-                    </Button>
-                    <br/><br/>
-                    <Button variant="primary" onClick={onDeleteAuction}>
-                        Cancel Auction (Delete)
-                    </Button>
-                </Form>
-            </Card.Body>
-        </Card>
+                                <Button variant="primary" onClick={onCreateAuction}>
+                                    Create auction
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card border="primary" style={{ width: '18rem' }}>
+                        <Card.Header>Auction: {applicationIndex}</Card.Header>
+                        <Card.Body>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Auction ID</Form.Label>
+                                    <Form.Control type="number" onChange={e => setApplicationIndex(parseInt(e.target.value))} placeholder="49055308" />
+                                </Form.Group>
+
+                                <Button variant="primary" onClick={onStartAuction}>
+                                    Start auction
+                                </Button>
+                                <br/><br/>
+                                <Button variant="primary" onClick={onOptIn}>
+                                    Opt In (no need)
+                                </Button>
+                                <br/><br/>
+                                <Button variant="primary" onClick={onClearAuction}>
+                                    Clear (not needed)
+                                </Button>
+                                <br/><br/>
+                                <Button variant="primary" onClick={onRepayAuction}>
+                                    Repay Auction + Delete
+                                </Button>
+                                <br/><br/>
+                                <Button variant="primary" onClick={onCancelAuction}>
+                                    Cancel Auction + Delete
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     </>);
 }
 
